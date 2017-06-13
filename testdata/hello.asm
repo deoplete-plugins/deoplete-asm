cf87c793a083665bbbb189f94e0aa02ba4befb82
Koichi Shiraishi <zchee.io@gmail.com>
2017-06-13 22:38:55 +0900

    testdata: fix hello asm test
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- testdata/hello.asm
+++ testdata/hello.asm
@@ -1,24 +1,43 @@
-; ----------------------------------------------------------------------------------------
-; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
-; To assemble and run:
-;
-;     nasm -felf64 hello.asm && ld hello.o && ./a.out
-; ----------------------------------------------------------------------------------------
+; http://peter.michaux.ca/articles/assembly-hello-world-for-os-x
+; nasm -f macho hello.asm
+; ld -o hello -e _start hello.o -lSystem
 
-        global  _start
+; hello.asm - a "hello, world" program using NASM
 
-        section .text
+section .text
+
+global _start                ; make the main function externally visible
+
 _start:
-        ; write(1, message, 13)
-        mov     rax, 1                  ; system call 1 is write
-        mov     rdi, 1                  ; file handle 1 is stdout
-        mov     rsi, message            ; address of string to output
-        mov     rdx, 13                 ; number of bytes
-        syscall                         ; invoke operating system to do the write
 
-        ; exit(0)
-        mov     eax, 60                 ; system call 60 is exit
-        xor     rdi, rdi                ; exit code 0
-        syscall                         ; invoke operating system to exit
-message:
-        db      "Hello, World", 10      ; note the newline at the end
+; 1 print "hello, world"
+
+    ; 1a prepare the arguments for the system call to write
+    push dword mylen          ; message length
+    push dword mymsg          ; message to write
+    push dword 1              ; file descriptor value
+
+    ; 1b make the system call to write
+    mov eax, 0x4              ; system call number for write
+    sub esp, 4                ; OS X (and BSD) system calls needs "extra space" on stack
+    int 0x80                  ; make the actual system call
+
+    ; 1c clean up the stack
+    add esp, 16               ; 3 args * 4 bytes/arg + 4 bytes extra space = 16 bytes
+
+; 2 exit the program
+
+    ; 2a prepare the argument for the sys call to exit
+    push dword 0              ; exit status returned to the operating system
+
+    ; 2b make the call to sys call to exit
+    mov eax, 0x1              ; system call number for exit
+    sub esp, 4                ; OS X (and BSD) system calls needs "extra space" on stack
+    int 0x80                  ; make the system call
+
+    ; 2c no need to clean up the stack because no code here would executed: already exited
+
+section .data
+
+  mymsg db "hello, world", 0xa  ; string with a carriage-return
+  mylen equ $-mymsg             ; string length in bytes
