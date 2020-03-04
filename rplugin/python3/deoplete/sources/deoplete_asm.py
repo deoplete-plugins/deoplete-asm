0a1526b8e0f08ea85a9af07e9fee2bdcf5c58d6a
Koichi Shiraishi <zchee.io@gmail.com>
2020-03-04 09:19:45 +0900

    sources: run black

--- rplugin/python3/deoplete/sources/deoplete_asm.py
+++ rplugin/python3/deoplete/sources/deoplete_asm.py
@@ -1,46 +1,56 @@
-import re
 import os
+import re
 
 from .base import Base
 from deoplete.util import error, load_external_module
 
-load_external_module(__file__, '')
+load_external_module(__file__, "")
 from exegesis.proto.pdf.x86 import intel_sdm_pb2 as intel_sdm  # noqa: E261
 
 
 class Source(Base):
-
     def __init__(self, vim):
         super(Source, self).__init__(vim)
 
-        self.name = 'asm'
-        self.mark = '[asm]'
+        self.name = "asm"
+        self.mark = "[asm]"
         self.rank = 500
-        self.filetypes = ['gas', 'asm', 'anm68k', 'asmh8300', 'ia64', 'masm', 'nasm', 'tasm']
+        self.filetypes = [
+            "goasm",
+            "gas",
+            "asm",
+            "anm68k",
+            "asmh8300",
+            "ia64",
+            "masm",
+            "nasm",
+            "tasm",
+        ]
+        self.input_pattern = "[^.'\" \t0-9]\\.\\w*"
 
         self.instructions = intel_sdm.SdmDocument()
         self.result = []  # for cache
 
     def on_init(self, context):
-        vars = context['vars']
-        self.go_mode = vars.get('deoplete#sources#asm#go_mode', False)
+        vars = context["vars"]
+        self.go_mode = vars.get("deoplete#sources#asm#go_mode", False)
 
-        pb = '{}/pb/instructions.sdm.pb'.format(os.path.dirname(__file__))
+        pb = "{}/pb/instructions.sdm.pb".format(os.path.dirname(__file__))
         try:
             f = open(pb, "rb")  # binary mode for predump protobuf data
             self.instructions.ParseFromString(f.read())
             f.close()
         except IOError:
-            error(self.vim, 'could not open {}'.format(pb))
+            error(self.vim, "could not open {}".format(pb))
 
     def get_complete_position(self, context):
-        m = re.search(r'\w*$', context['input'])
+        m = re.search(r"\w*$", context["input"])
         return m.start() if m else -1
 
     def gather_candidates(self, context):
         if not self.result:
             if self.go_mode:
-                from .opcode import go
+                from sources.opcode import go
 
                 self.result += go.symbols
 
@@ -52,25 +62,23 @@ class Source(Base):
                     mnemonic = str(vendor_syntax.mnemonic).lower()
                     if self.go_mode:
                         try:
-                            kind = '({}) {}'.format(mnemonic, kind)
+                            kind = "({}) {}".format(mnemonic, kind)
                             mnemonic = go.mnemonics[mnemonic]
                         except Exception:
                             continue
 
-                    operand = ''
+                    operand = ""
                     for i, op in enumerate(vendor_syntax.operands):
                         operand += op.name
                         if i < len(vendor_syntax.operands) - 1:
-                            operand += ', '
-                    abbr = '{} {}'.format(mnemonic, operand)
+                            operand += ", "
+                    abbr = "{} {}".format(mnemonic, operand)
+
+                    if instructions.feature_name:
+                        abbr += " ({})".format(instructions.feature_name)
 
                     self.result.append(
-                        dict(
-                            word=mnemonic,
-                            abbr=abbr,
-                            kind=kind,
-                            info=abbr,
-                            dup=1)
+                        dict(word=mnemonic, abbr=abbr, kind=kind, info=abbr, dup=1)
                     )
 
         return self.result
